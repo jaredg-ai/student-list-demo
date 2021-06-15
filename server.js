100234dc0bc5d06811200f152c9ed05a9c005fcb
jaredg-ai <jaredward.harod@gmail.com>
2021-06-15 10:05:02 -0600

    added post method

--- server.js
+++ server.js
@@ -9,10 +9,18 @@ let rollbar = new Rollbar({
 
 
 const app = express()
+let students = []
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully')
+})
+
+app.post('/api/student', (req, res) => {
+    let {name} = req.body
+    name = name.trim()
+    students.push(name)
+    rollbar.log('student added successfully', {author: 'jared', type: 'manual'})
 })
 
 const port = process.env.PORT || 4545
